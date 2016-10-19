9e59b6aabc7e667a07cb1e44cb311a9a2dd7b830
Borja Fabregas <borjafabregas.16@campuscamara.es>
2016-10-19 13:44:33 +0200

    Update LeeFichero.java

--- FicheroAleatorio/src/LecturaFichero/LeeFichero.java
+++ FicheroAleatorio/src/LecturaFichero/LeeFichero.java
@@ -35,10 +35,11 @@ public class LeeFichero {
                     cadApe[i] = raf.readChar();
                 }
                 apellidos = new String(cadApe);
-                //apellidos=apellidos.trim() hace lo mismo que el if 
-                if (apellidos.indexOf('\u0000') != -1) {
+                apellidos=apellidos.trim();
+                
+                /*if (apellidos.indexOf('\u0000') != -1) {
                     apellidos = apellidos.substring(0, apellidos.indexOf('\u0000'));
-                }
+                }*/
 
                 //Lee deptNo
                 deptNo = raf.readByte();
